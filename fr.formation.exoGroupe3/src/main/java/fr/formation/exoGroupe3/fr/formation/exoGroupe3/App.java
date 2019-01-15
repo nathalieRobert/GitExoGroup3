70ad40de650fbf378e129a1d0b1cd48ebf414d7e
Nathalie ROBERT <robert.nathaliejm@gmail.com>
2019-01-15 20:14:30 +0100

    Initial commit

---
+++ fr.formation.exoGroupe3/src/main/java/fr/formation/exoGroupe3/fr/formation/exoGroupe3/App.java
@@ -0,0 +1,13 @@
+package fr.formation.exoGroupe3.fr.formation.exoGroupe3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
